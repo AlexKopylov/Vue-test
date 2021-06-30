0e7c576841ef5a09d8bfa720ebe1da6ab52a5152
AlexKopylov <alex.kopylov.08@gmail.com>
2021-06-30 20:48:07 +0300

    c1: first commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <tree
+      :node="root"
+      @make-folder="makeFolder"
+      @add-item="addItem"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import tree from "./components/tree";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      root: {
+        name: 'Food',
+        children: [
+          {
+            name: "Plant",
+            children: [
+              {
+                name: "Fruits",
+                children: [
+                  {name: "hello"},
+                  {name: "wat"}]
+              },
+              {
+                name: "Vegetables",
+                children: [
+                  {name: "hello"},
+                  {name: "wat"}
+                ]
+              },
+            ]
+          },
+          {
+            name: "Animal",
+            children: [
+              {
+                name: "child folder",
+                children: [{name: "hello"}, {name: "wat"}]
+              },
+            ]
+          },
+          {
+            name: "child folder",
+            children: [
+              {
+                name: "child folder",
+                children: [{name: "hello"}, {name: "wat"}]
+              },
+              {name: "hello"},
+              {name: "wat"},
+              {
+                name: "child folder",
+                children: [{name: "hello"}, {name: "wat"}]
+              }
+            ]
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    makeFolder: function (item) {
+      this.$set(item, "children", []);
+      this.addItem(item);
+    },
+    addItem: function (item) {
+      item.children.push({
+        name: "new Child"
+      })
+    }
+  },
   components: {
-    HelloWorld
+    tree
   }
 }
 </script>
 
 <style>
 #app {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/tree.vue
@@ -0,0 +1,159 @@
+<!-- eslint-disable -->
+<template>
+  <div class="">
+    <div
+        :style="{'margin-left': `${depth * 20}px`}"
+        @dblclick="makeFolder"
+
+        class="node">
+      <span
+          class="node-arrow type"
+          @click="toggle"
+          v-if="isFolder"
+          >
+        [{{ isOpen ? '&#9660;' : '&#9658;' }}]
+      </span>
+      {{ node.name }}
+      <button
+          class="btn"
+          @click="$emit('add-item', node)"
+      ></button>
+    </div>
+    <ul class="node__list" v-show="isOpen" v-if="isFolder">
+      <tree
+          class="node__item"
+          v-for="(child, index) in node.children"
+          :key="index"
+          :node="child"
+          :depth="depth + 1"
+          make-folder="$emit('make-folder', $event)"
+          @add-item="$emit('add-item', $event)"
+      />
+      <li>
+        <button
+            class="btn"
+            @click="$emit('add-item', node)"
+        ></button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tree',
+  props: {
+    node: Object,
+    depth: {
+      type: Number,
+      default: 0
+    }
+  },
+  data: function() {
+    return {
+      isOpen: false
+    };
+  },
+  computed: {
+    // hasChildren() {
+    //   return this.node.children;
+    // },
+    isFolder: function() {
+      return this.node.children && this.node.children.length;
+    }
+  },
+  methods: {
+    toggle: function() {
+      if (this.isFolder) {
+        this.isOpen = !this.isOpen;
+      }
+    },
+    makeFolder: function() {
+      if (!this.isFolder) {
+        this.$emit("make-folder", this.item);
+        this.isOpen = true;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.node {
+  padding: 1rem;
+  background-color: #34cde3;
+  border-radius: 1rem;
+  width: 10rem;
+  margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  z-index: 0;
+}
+
+
+.node__item {
+  position: relative;
+}
+
+.node__list {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  list-style-type: none;
+  justify-content: flex-start;
+}
+.node__list li  {
+
+}
+.node__list .btn {
+  left: 0;
+}
+
+.node-arrow {
+  display: block;
+  width: 1rem;
+  height: 1rem;
+  margin-right: .5rem;
+  position: relative;
+  top: -3px;
+}
+
+.btn {
+  background-color: transparent;
+  border-radius: 50%;
+  padding: .5rem;
+  border: 1px solid;
+  width: 1.5rem;
+  height: 1.5rem;
+  position: relative;
+  left: 95%;
+  z-index: 1;
+  cursor: pointer;
+  margin-left: 1rem;
+}
+
+.btn::before {
+  content: '';
+  width: 1rem;
+  height: 2px;
+  background-color: #000;
+}
+
+.btn::after {
+  content: '';
+  height: 1rem;
+  width: 2px;
+  background-color: #000;
+}
+
+.btn::before,
+.btn::after {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
+<!-- eslint-enable -->
